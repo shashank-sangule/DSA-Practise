2974fe9c34ea2a6ce3e4803b7a783f76fd449d02
Shashank Sangule <sanguleshashank@gmail.com>
2022-07-12 19:54:14 +0530

    2 Array questions solved.

---
+++ Array/Find_minimum_and_maximum_element_in_an_array.cpp
@@ -0,0 +1,43 @@
+/*
+Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
+Input:
+N = 6
+A[] = {3, 2, 1, 56, 10000, 167}
+Output:
+min = 1, max =  10000
+*/
+
+#include <iostream>
+
+std::pair<int, int> findMaxMin(int arr[], int size)
+{
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for (int i = 0; i < size; i++)
+    {
+        min = std::min(arr[i], min);
+        max = std::max(arr[i], max);
+    }
+    std::pair<int, int> ans;
+    ans.first = min;
+    ans.second = max;
+    return ans;
+}
+
+int main()
+{
+    int arr[100];
+    int size;
+    std::cout << "Enter the size of array: ";
+    std::cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter the value of " << i << " index: ";
+        std::cin >> arr[i];
+    }
+
+    std::pair<int, int> ans = findMaxMin(arr, size);
+    std::cout << "Minimum value in the Array is: " << ans.first << std::endl;
+    std::cout << "Maximum value in the Array is: " << ans.second << std::endl;
+}
---
+++ Array/Reverse_an_Array.cpp
@@ -0,0 +1,63 @@
+/*
+
+Reverse an Array
+
+Given an array A of size N, print the reverse of it.
+
+Example:
+
+Input:
+1
+4
+1 2 3 4
+Output:
+4 3 2 1
+
+*/
+
+#include <iostream>
+
+void reverse(int arr[], int size)
+{
+    int start = 0,
+        end = size - 1;
+    for (int i = 0; i < size; i++)
+    {
+        if (start < end)
+        {
+            std::swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+void printArr(int arr[], int size)
+{
+    std::cout << "Reversed Array: " << std::endl;
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    int arr[100];
+    int size;
+    std::cout << "Enter the size of array: ";
+    std::cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter the value of " << i << " index: ";
+        std::cin >> arr[i];
+    }
+
+    reverse(arr, size);
+    printArr(arr, size);
+}
